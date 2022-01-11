5fb5920746abd539b345d1eab186ae956295e890
Viktoriia Huk <relax16011991@gmail.com>
2022-01-11 22:04:06 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from "react";
+import { nanoid } from "nanoid";
+import Form from "./components/Form/Form";
+import ContactsList from "./components/ContactsList/ContactsList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    contacts: [
+      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+    ],
+    name: "",
+    number: "",
+  };
+
+  addContact = ({ name, number }) => {
+    const { contacts } = this.state;
+
+    const newContact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+
+    contacts.find(
+      (contact) => newContact.name.toLowerCase() === contact.name.toLowerCase()
+    )
+      ? alert(`${newContact.name} is already in contacts`)
+      : this.setState(({ contacts }) => ({
+          contacts: [...contacts, newContact],
+        }));
+  };
+
+  render() {
+    return (
+      <div>
+        <Form onaddContact={this.addContact} />
+        <ContactsList />
+      </div>
+    );
+  }
 }
 
 export default App;
